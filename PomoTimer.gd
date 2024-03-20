b043c7fc739009b849d6f153c73e89a7b8008d7d
samrobert7 <srobert7@macalester.edu>
2024-03-20 15:18:11 -0500

    Fiddling with timer

--- PomoTimer.gd
+++ PomoTimer.gd
@@ -1,11 +1,27 @@
 extends Node
 
+const MINUTE = 60.0
+var time = 0.0
+var timeLimit = 20 * MINUTE
+var timerRunning = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if timerRunning && time <= timeLimit:
+		time += delta
+
+func getTime():
+	return time
+
+func isRunning():
+	return timerRunning
+
+func resetTime():
+	time = 0.0
+
+func toggleRunning():
+	timerRunning = !timerRunning
