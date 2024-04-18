0ae8a4b4db3602f9208996ff46e53dddf13455dc
James-J-McCarthy <112642209+James-J-McCarthy@users.noreply.github.com>
2024-04-17 19:33:21 -0500

    break readout text fixed

---
+++ BreakReadout.gd
@@ -0,0 +1,8 @@
+extends Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_break_time_slider_value_changed(value):
+	set_text(str(value)) # Replace with function body.
